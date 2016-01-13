84340c4bd44678900811c6ddbb5cee599e742a39
Vladimir37 <vladimir_zapas@mail.ru>
2016-01-13 22:23:12 +0000

    Basis 0.7.2

--- app/basis/api/get_tasks.js
+++ app/basis/api/get_tasks.js
@@ -43,7 +43,42 @@ function active_tasks(req, res, next) {
 
 //get inactive tasks for user
 function inactive_tasks(req, res, next) {
-    //
+    //author data
+    var author = res.user_status.id;
+    var room = res.user_status.room;
+    var author_group = res.user_status.group;
+    //personal
+    if(!room) {
+        db.tasks.findAll({
+            where: {
+                user: author,
+                active: 0
+            }
+        }).then(function(result) {
+            res.end(serializing(0, result));
+        }, function(err) {
+            console.log(err);
+            res.end(serializing(1));
+        });
+    }
+    //company
+    else {
+        db.tasks.findAll({
+            where: {
+                room,
+                active: 0,
+                $or: {
+                    u_group: author_group,
+                    performer: author
+                }
+            }
+        }).then(function(result) {
+            res.end(serializing(0, result));
+        }, function(err) {
+            console.log(err);
+            res.end(serializing(1));
+        });
+    }
 };
 
 exports.active_tasks = active_tasks;
