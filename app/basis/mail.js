9ae89e6612af5b816971cdfb6254990e4cf97df7
Vladimir37 <vladimir_zapas@mail.ru>
2016-01-14 19:47:56 +0000

    Mail 0.2

--- app/basis/mail.js
+++ app/basis/mail.js
@@ -1,9 +1,9 @@
 var nodemailer = require('nodemailer');
 var jade = require('jade');
 
-var config = require('../../config/mail_data');
+var config = require('../../config/mail');
 
-//Указание данных подключения
+//Connection data
 var transporter = nodemailer.createTransport({
     service: config.service,
     auth: {
@@ -13,34 +13,32 @@ var transporter = nodemailer.createTransport({
 });
 
 //Функция отправки
-function send(address, subject, text) {
-    //Данные отправки
-    var mailOptions = {
-        from: config.from, //от кого
-        to: address, //кому
-        subject: subject, //тема
-        html: text //HTML письма
-    };
-    //Результат отправки
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error) {
-            console.log('Ошибка при отправке письма: ' + error);
-        }
-        else {
-            console.log('Письмо успешно отправлено: ' + info.response);
-        }
-    });
-};
-
-function template(address, subject, name, obj) {
-    jade.renderFile('mails/' + name + '.jade', obj, function(err, result) {
+function send(name, address, subject, text, obj) {
+    obj = obj || {};
+    //read letter
+    jade.renderFile('client/view/mail/' + name + '.jade', obj, function(err, result) {
         if(err) {
             console.log(err);
         }
         else {
-            send(address, subject, result);
+            //Send data
+            var mailOptions = {
+                from: config.from,
+                to: address,
+                subject: subject,
+                html: text
+            };
+            //Send result
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error) {
+                    console.log('Send error: ' + error);
+                }
+                else {
+                    console.log('Success send: ' + info.response);
+                }
+            });
         }
     });
 };
 
-module.exports = template;
+module.exports = send;
