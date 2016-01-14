d033e38981223597cebf5625db8ff28d0c6dc021
Vladimir37 <vladimir_zapas@mail.ru>
2016-01-14 00:32:32 +0000

    Basis 0.7.4

--- app/basis/api/manage_data.js
+++ app/basis/api/manage_data.js
@@ -2,7 +2,7 @@ var db = require('../database');
 var serializing = require('../serializing');
 
 //get user groups
-function user_group(req, res, next) {
+function users_group(req, res, next) {
     //author data
     var author = res.user_status.id;
     var room = res.user_status.room;
@@ -40,8 +40,67 @@ function user_group(req, res, next) {
 
 //get all users in room
 function users(req, res, next) {
-    //
+    //author data
+    var author = res.user_status.id;
+    var room = res.user_status.room;
+    var author_group = res.user_status.group;
+    //right to view all users
+    var view_right = false;
+    author_group == 0 ? view_right = true : view_right = false;
+    db.users_groups.findById(author_group).then(function(group) {
+        if(!group) {
+            throw '1';
+        }
+        group.u_group_manage == 1 ? view_right = true : view_right = false;
+        return Promise.resolve();
+    }).catch(function(err) {
+        console.log(err);
+        if(view_right) {
+            return Promise.resolve();
+        }
+        else {
+            res.end(serializing(1));
+        }
+    }).then(function() {
+        return db.users.findAll({
+            where: {
+                room
+            }
+        });
+    }).then(function(users) {
+        res.end(serializing(0, users));
+    }).catch(function(err) {
+        console.log(err);
+        res.end(serializing(1));
+    });
 };
 
-exports.user_group = user_group;
+//get all task groups in room
+function tasks_group(req, res, next) {
+    //author data
+    var author = res.user_status.id;
+    var room = res.user_status.room;
+    var author_group = res.user_status.group;
+    //right to view all users
+    var view_right = false;
+    author_group == 0 ? view_right = true : view_right = false;
+    if(!room) {
+        db.tasks_groups.findAll({
+            where: {
+                user: author
+            }
+        }).then(function(groups) {
+            res.end(serializing(0, groups));
+        }, function(err) {
+            console.log(err);
+            res.end(serializing(1));
+        });
+    }
+    else {
+        //
+    }
+};
+
+exports.users_group = users_group;
 exports.users = users;
+exports.tasks_groups = tasks_group;
--- app/router/api/manage_data.js
+++ app/router/api/manage_data.js
@@ -6,9 +6,12 @@ var manage_data = require('../../basis/api/manage_data');
 var router = express.Router();
 
 //get all user groups in room
-router.get('/user_group', status.medium, manage_data.user_group);
+router.get('/users_group', status.medium, manage_data.users_group);
 
 //get all users in room
 router.get('/users', status.medium, manage_data.users);
+
+//get all tasks group
+router.get('/tasks_groups', status.hard, manage_data.tasks_groups);
 
 module.exports = router;
