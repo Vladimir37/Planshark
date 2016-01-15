ef9db9216a0ae08a23d7cb2cd8f3f24576d7b2ad
Vladimir37 <vladimir_zapas@mail.ru>
2016-01-15 21:14:41 +0000

    Front 0.1.3

--- client/source/js/index.js
+++ client/source/js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('.content').on('click', '.index_but:not(.active)', function() {
-        console.log(12);
         $('.index_but').toggleClass('active');
         var visible_form = $('.index_form:visible');
         var hidden_form = $('.index_form:hidden');
