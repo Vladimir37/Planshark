2b405ae33357db45c831300b9e80788883da241f
Vladimir37 <vladimir_zapas@mail.ru>
2016-01-16 03:24:04 +0000

    Front 0.1.4

--- client/source/js/submitting.js
+++ client/source/js/submitting.js
@@ -1,22 +1,20 @@
 //submitting form on press button
-$(document).ready(function() {
-    $('button.sub').click(function () {
-        var form_data = {};
-        var form = $(this).parent();
-        var addr = form.data('addr');
-        var request_type = form.data('type');
-        var text_inputs = form.find('input[type="text"], input[type="password"], textarea');
-        var radio_inputs = form.find('input[type="radio"]:checked');
-        var checkboxes = form.find('input[type="checkbox"]:checked');
-        for(var i = 0; i < text_inputs.length; i++) {
-            form_data[text_inputs[i].name] = text_inputs[i].value;
-        }
-        for(var i = 0; i < radio_inputs.length; i++) {
-            form_data[radio_inputs[i].name] = radio_inputs[i].value;
-        }
-        for(var i = 0; i < checkboxes.length; i++) {
-            form_data[checkboxes[i].name] = true;
-        }
-        console.log(form_data);
-    });
-});
+function submitting(success, error, process, done) {
+    var form_data = {};
+    var form = $(this).parent();
+    var addr = form.data('addr');
+    var request_type = form.data('type');
+    var text_inputs = form.find('input[type="text"], input[type="password"], textarea');
+    var radio_inputs = form.find('input[type="radio"]:checked');
+    var checkboxes = form.find('input[type="checkbox"]:checked');
+    for (var i = 0; i < text_inputs.length; i++) {
+        form_data[text_inputs[i].name] = text_inputs[i].value;
+    }
+    for (var i = 0; i < radio_inputs.length; i++) {
+        form_data[radio_inputs[i].name] = radio_inputs[i].value;
+    }
+    for (var i = 0; i < checkboxes.length; i++) {
+        form_data[checkboxes[i].name] = true;
+    }
+    console.log(form_data);
+};
