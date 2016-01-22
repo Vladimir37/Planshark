bdad22b468ef5f6aac614bbd9436bfeca0c1b1f3
Vladimir37 <vladimir_zapas@mail.ru>
2016-01-22 03:11:30 +0000

    Basis 0.7.7

--- app/router/index.js
+++ app/router/index.js
@@ -7,4 +7,16 @@ var router = express.Router();
 //index page
 router.get('/', render('main/index'));
 
+//tasks manage
+router.get('/tasks', render('main/index'));
+
+//tasks groups manage
+router.get('/tasks_groups', render('main/index'));
+
+//users groups manage
+router.get('/users_groups', render('main/index'));
+
+//users manage
+router.get('/users', render('main/index'));
+
 module.exports = router;
